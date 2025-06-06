e6dae1096edc9db0c6660105748082aa5592d9b4
Pawan Bangar <pawanbangar5360@gmail.com>
2025-06-06 10:41:39 +0530

    added supabase conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import eslint from "vite-plugin-eslint";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env": env,
+    },
+    plugins: [react(),eslint({ cache: false }), // disable cache if linting doesn't update
+    ],
+  };
+});
